76a1926f0bca19d499b3fc39c4e495a58966e400
kirildobrev919 <kirildobrev919@gmail.com>
2020-06-28 22:48:05 +0100

    Business object passed through props

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,14 +5,37 @@ import './App.css';
 import BusinesList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinesList />
-    </div>
-  );
+var business = {
+  imagesrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinesList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
