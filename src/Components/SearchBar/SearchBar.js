8e646d255fb3ba6f84c831c5ff349ceb2fdef94c
kirildobrev919 <kirildobrev919@gmail.com>
2020-07-04 21:38:50 +0100

    Added functionallity for search term, location, sort and the button

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -15,6 +15,11 @@ class SearchBar extends React.Component {
             location: '',
             sortBy: 'best_match'
         };
+
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+
         this.sortByOptions = {
             'Best Match': 'best_match',
             'Highest Rated': 'rating',
@@ -34,6 +39,27 @@ class SearchBar extends React.Component {
         this.setState({
             sortBy: sortByOption
         })
+    };
+
+    handleTermChange(event) {
+      this.setState({
+        term: event.target.value
+      });
+    };
+
+    handleLocationChange(event) {
+      this.setState({
+        location: event.target.value
+      });
+    };
+
+    handleSearch(event) {
+      this.props.searchYelp(
+        this.state.term,
+        this.state.location,
+        this.state.sortBy
+      );
+      event.preventDefault();
     };
 
   renderSortByOptions() {
@@ -60,11 +86,11 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+          <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-          <a>Let's Go</a>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
