6e8d2b0ea24e6b8a263a4a2393b8513fcaf44d8a
kirildobrev919 <kirildobrev919@gmail.com>
2020-07-06 23:03:49 +0100

    async call using fetch to Yelp API introduced

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,9 +1,32 @@
 const apiKey = 'UZNVWNXu_jzZtwbRVzaS1XX2Rnt3cm53cGD5T8jjwsx8WzRLK5aSwbspP68Whx62tVs45RjUQWZe6X1D_9YhFHYoCJ74s8KHgQ5iOe2qgbAP1FgdruA1MjAFskYCX3Yx';
 
 const Yelp = {
-
+    search = async (term, location, sortBy) => {
+        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`
+                }
+            });
+        const jsonResponse = await response.json();
+        if (jsonResponse.businesses) {
+            jsonResponse.businesses.map(business => {
+                console.log(business);
+                return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zipCode,
+                    category: business.category[0].title,
+                    rating: business.rating,
+                    reviewCount: business.reviewCount
+                };
+            });
+        }
+    }
 }
 
-let search = (term, location, sortBy) => {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`)
-}
+export default Yelp;
