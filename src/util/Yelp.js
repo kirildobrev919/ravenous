b8d257bf7e0c9a6540733c24625d95ecff2ea1fc
kirildobrev919 <kirildobrev919@gmail.com>
2020-07-07 23:33:05 +0100

    Fixes to Yelp search fetch call. Did not worked properly

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,31 +1,38 @@
 const apiKey = 'UZNVWNXu_jzZtwbRVzaS1XX2Rnt3cm53cGD5T8jjwsx8WzRLK5aSwbspP68Whx62tVs45RjUQWZe6X1D_9YhFHYoCJ74s8KHgQ5iOe2qgbAP1FgdruA1MjAFskYCX3Yx';
+const cors = 'https://cors-anywhere.herokuapp.com/';
 
 const Yelp = {
-    async search (term, location, sortBy) {
-        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    search(term, location, sortBy) {
+        debugger;
+        return fetch(`${cors}https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
             {
                 headers: {
-                    Authorization: `Bearer ${apiKey}`
+                    Authorization: `Bearer ${apiKey}`,
+                },
+            }).then((response) => {
+                debugger;
+                return response.json();
+            }).then((jsonResponse) => {
+                if (jsonResponse.businesses) {
+                    debugger;
+                    jsonResponse.businesses.map(business => {
+                        console.log(business);
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zipCode,
+                            category: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount: business.reviewCount
+                        };
+                    });
                 }
-            });
-        const jsonResponse = await response.json();
-        if (jsonResponse.businesses) {
-            jsonResponse.businesses.map(business => {
-                console.log(business);
-                return {
-                    id: business.id,
-                    imageSrc: business.image_url,
-                    name: business.name,
-                    address: business.location.address1,
-                    city: business.location.city,
-                    state: business.location.state,
-                    zipCode: business.location.zipCode,
-                    category: business.categories[0].title,
-                    rating: business.rating,
-                    reviewCount: business.reviewCount
-                };
-            });
-        }
+            }
+        );
     }
 }
 
