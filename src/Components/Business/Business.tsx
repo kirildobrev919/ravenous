1bbf2ad64a0f84bec8025ad7fe695305da4eab4c
Kiril Dobrev <kirildobrev919@gmail.com>
2026-02-22 00:31:12 +0000

    PF-04 Transform Business.js to tsx

--- src/Components/Business/Business.tsx
+++ src/Components/Business/Business.tsx
@@ -1,33 +1,38 @@
 import * as React from 'react';
 import './Business.css';
+import { MappedBusiness } from '../../util/types';
 /// <reference path="react.d.ts" />
 /* this is just test of using typescript in react
 it should not use Business.js file while .tsx is imported in BusinessList.js */
 
-class Business extends React.Component<any, {}> {
-
-    render() {
-        return (
-            <div className="Business">
-                <div className="image-container">
-                    <img src={this.props.business.imageSrc} alt='The venue' />
-                </div>
-                <h2>{this.props.business.name}</h2>
-                <div className="Business-information">
-                    <div className="Business-address">
-                        <p>{this.props.business.address}</p>
-                        <p>{this.props.business.city}</p>
-                        <p>{this.props.business.state} {this.props.business.zipCode}</p>
-                    </div>
-                    <div className="Business-reviews">
-                        <h3>{this.props.business.category}</h3>
-                        <h3 className="rating">{this.props.business.rating} stars</h3>
-                        <p>{this.props.business.reviewCount} reviews</p>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+type BusinessType ={
+    key: any,
+    business: MappedBusiness,
 }
+
+const Business = (props: BusinessType) => {
+  return (
+    <div className="Business">
+      <div className="image-container">
+        <img src={props.business.imageSrc} alt="The venue" />
+      </div>
+      <h2>{props.business.name}</h2>
+      <div className="Business-information">
+        <div className="Business-address">
+          <p>{props.business.address}</p>
+          <p>{props.business.city}</p>
+          <p>
+            {props.business.state} {props.business.zipCode}
+          </p>
+        </div>
+        <div className="Business-reviews">
+          <h3>{props.business.category}</h3>
+          <h3 className="rating">{props.business.rating} stars</h3>
+          <p>{props.business.reviewCount} reviews</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Business;
